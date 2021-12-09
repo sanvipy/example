0ff7a79a130a462e33edc4907a90975c837e18ab
sanvi <sanjupy06@gmail.com>
2021-12-09 11:58:31 +0800

    Create main.tf

---
+++ hello/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "ap-southeast-1"    
+  profile = "mdx-demo"          
+}
+
+// Lambda IAM role
+resource "aws_iam_role" "lambda_role" {
+  name = "hellogoFunctionRole"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "lambda.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+// Lambda function
+resource "aws_lambda_function" "function" {
+  filename      = "deployment.zip"
+  function_name = "hello-go"
+  role          = "${aws_iam_role.lambda_role.arn}"
+  handler       = "main"
+  runtime       = "go1.x"
+}
